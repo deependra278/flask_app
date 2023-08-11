45f0239339b62e78b2ab35b8d2801890a62d5682
Deependra Kumar <deependra.kumar@lilly.com>
2023-08-11 12:35:55 +0530

    Commiting first flask app

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask,render_template
+
+ 
+
+app = Flask(__name__)
+
+ 
+
+@app.route("/")
+
+def hello_world():
+
+    number=[1,2,3,4,5]
+
+    return render_template(("index.html"),number=number)
+
+ 
+
+if __name__=="__main__":
+
+    app.run(debug=True)
